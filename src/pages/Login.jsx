531af57473a2c5adfcac96f77b0a1672b48d44c9
Carlos Afonso <carlos.flach15@gmail.com>
2021-09-15 17:17:06 -0300

    Create Loading component, an async function to setState and a conditional redirection.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends Component {
   constructor() {
     super();
     this.state = {
       inputLoginName: '',
+      loading: false,
+      loaded: false,
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   handleChange({ target }) {
@@ -17,16 +23,31 @@ class Login extends Component {
     });
   }
 
-  render() {
+  async handleClick() { // Setting an async funcion based on Écio Ferraz repository
+    this.setState({
+      loading: true,
+    });
     const { inputLoginName } = this.state;
+    await createUser({ name: inputLoginName });
+    this.setState({
+      loading: false,
+      loaded: true,
+    });
+  }
+
+  render() {
+    const { inputLoginName, loaded, loading } = this.state;
     const MIN_CHARACTER = 3;
+    if (loading) return <Loading />;
+    if (loaded) return <Redirect to="/search" />;
     return (
       <div data-testid="page-login">
         <form>
-          <label htmlFor="inputLoginName">
+          <label htmlFor="login">
             User:
-            <input // O valor de qualquer variável sempre fica salvo no estado. Lembrar de setar o estado, e a função que modifica o valor.
+            <input // Remember to set the initial state, and create a function to update the state.
               type="text"
+              placeholder="Name"
               name="inputLoginName"
               id="login"
               data-testid="login-name-input"
@@ -35,8 +56,10 @@ class Login extends Component {
             <button
               type="button"
               disabled={ inputLoginName.length < MIN_CHARACTER }
+              // Source: https://stackoverflow.com/questions/41488715/how-to-disable-button-in-react-js
               // Se o valor armazenado no estado do input for maior que 3 habilita o botão.
               data-testid="login-submit-button"
+              onClick={ this.handleClick }
             >
               Entrar
             </button>
