af5a5d9b88398cae4d4b2a2521091fe41a343d49
Carlos Afonso <carlos.flach15@gmail.com>
2021-09-15 16:14:59 -0300

    Create tags form, input and button. Also adds the initial state, and function to set new state on change.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,47 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputLoginName: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { inputLoginName } = this.state;
+    const MIN_CHARACTER = 3;
     return (
       <div data-testid="page-login">
-        quote
+        <form>
+          <label htmlFor="inputLoginName">
+            User:
+            <input // O valor de qualquer variável sempre fica salvo no estado. Lembrar de setar o estado, e a função que modifica o valor.
+              type="text"
+              name="inputLoginName"
+              id="login"
+              data-testid="login-name-input"
+              onChange={ this.handleChange }
+            />
+            <button
+              type="button"
+              disabled={ inputLoginName.length < MIN_CHARACTER }
+              // Se o valor armazenado no estado do input for maior que 3 habilita o botão.
+              data-testid="login-submit-button"
+            >
+              Entrar
+            </button>
+          </label>
+        </form>
       </div>
     );
   }
